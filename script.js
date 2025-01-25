7082d590acd4be9ec27d0da76a4485197f14ad97
PrashanjeetChakrabarty <prashanjeetchakrabarty@gmail.com>
2025-01-25 15:29:59 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+let sections = document.querySelectorAll('section');
+
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+    
+}
